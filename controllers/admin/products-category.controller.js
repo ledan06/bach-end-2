6b5f2495362d52882c85a72fe97db7fd2a833069
ledan06 <webbackend123@gmail.com>
2024-06-09 17:07:14 +0700

    update product-category

--- controllers/admin/products-category.controller.js
+++ controllers/admin/products-category.controller.js
@@ -1,19 +1,116 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system")
 
+const filterStatusHelpers = require("../../helpers/filterStatus")
+const searchHelpers = require("../../helpers/search")
+const paginationHelpers = require("../../helpers/pagination")
+
 //[GET] /admin/products-category
 module.exports.index = async(req, res) => {
+
+    const filterStatus = filterStatusHelpers(req.query);
+    const objectSearch = searchHelpers(req.query);
+
     let find={
         deleted: false
     }
 
+    //Status
+    if(req.query.status){
+        find.status = req.query.status
+    };
+
+    //Search
+    if(objectSearch.regex){
+        find.title = objectSearch.regex
+    };
+
+    //Pagination
+    const countProducts = await ProductCategory.countDocuments(find);
+    let objectPagination = paginationHelpers (
+        {
+        currentPage: 1,
+        limitItems: 4
+        },
+        req.query,
+        countProducts
+    );
+   
+
     const records = await ProductCategory.find(find)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
+
     res.render("admin/pages/products-category/index", {
         pageTitle: "Danh mục sản phẩm",
-        records: records
+        records: records,
+        filterStatus: filterStatus,
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 }
 
+//[Patch] /admin/products-category/change-status/:status/:id
+module.exports.changeStatus = async(req, res) => {
+    const status = req.params.status
+    const id = req.params.id;
+    
+    await ProductCategory.updateOne({ _id: id }, { status: status });
+    //updateOne là hàm của mongoos để update 1 sản phẩm
+
+    req.flash("success", "Cập nhật trạng thái thành công!")
+    res.redirect("back");
+}
+
+//[PATCH] /admin/products-category/change-/change-multi
+module.exports.changeMulti = async (req, res) => {
+    const type = req.body.type;
+    const ids = req.body.ids.split(", ");
+ 
+    switch (type) {
+     case "active":
+         await ProductCategory.updateMany({ _id: { $in: ids} }, { status: "active"});
+         req.flash("success", "Cập nhật trạng thái thành công!")
+         break;
+     case "inactive":
+         await ProductCategory.updateMany({ _id: { $in: ids} }, { status: "inactive"}); 
+         req.flash("success", "Cập nhật trạng thái thành công!")
+         break;
+    case "delete-all":
+        await ProductCategory.updateMany({ _id: { $in: ids} }, {
+            deleted: true,
+            deletedAt: new Date()
+        }); 
+        req.flash("success", " Đã xóa thành công!")
+        break;
+    case "change-position":
+        for (const item of ids) {
+            
+            let [id, position] = item.split("-")
+            position = parseInt(position)
+            await ProductCategory.updateOne({ _id: id }, {
+                position: position 
+            });
+            req.flash("success", "Cập nhật vị trí thành công!")
+            // req.flash("success", `Đã đổi vị trí thành công ${ids.length} sản phẩm!`)
+        }
+        break;
+     default:
+         break;
+    }
+    res.redirect("back");
+ }
+
+//[DELETE] /admin/product-category/delete/id
+module.exports.deleteItems = async (req, res) => {
+    const id = req.params.id;
+    await ProductCategory.updateOne({ _id: id }, {
+        deleted: true,
+        deletedAt: new Date()
+    });
+    req.flash("success", "Đã xóa thành công!")
+    res.redirect("back");
+}
 //[GET] /admin/products-category/create
 module.exports.create = async(req, res) => {
 
--- routes/admin/products-category.route.js
+++ routes/admin/products-category.route.js
@@ -9,6 +9,11 @@ const validate = require("../../validates/admin/product-category.validate");
 const uploadCloud = require("../../middlewares/admin/uploadCloud.middlewares");
 
 router.get('/', controller.index);
+router.patch('/change-status/:status/:id', controller.changeStatus);
+router.patch('/change-multi', controller.changeMulti);
+
+router.delete('/delete/:id',controller.deleteItems)
+
 router.get('/create', controller.create);
 router.post('/create', 
     upload.single('thumbnail'), 
